cfc3b1b87f06c8b5ff4cc739fe634fd3588bb698
Egor <sokolovegor1998@gmail.com>
2021-12-16 15:05:52 +0300

    coin

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,11 +2,13 @@ package com.company;
 
 
 import java.io.*;
+import java.util.ArrayList;
 
 
 public class Main {
 
     public static void main(String[] args) {
+
         InputStream inputStream = System.in;
         Reader inputStreamReader = new InputStreamReader(inputStream);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
@@ -21,26 +23,59 @@ public class Main {
 
         assert name != null;
         long quantityAttempt = Long.parseLong(name);
+        ArrayList <Integer> basketCoins = new ArrayList<>();
 
         long quantityOrel = 0;
         long quantityReshka = 0;
 
-        for(long i = 0; i < quantityAttempt; i++){
-            if(Math.random() < 0.5) {
+        for (long i = 0; i < quantityAttempt; i++) {
+            if (Math.random() < 0.5) {
                 quantityReshka++;
-            } else quantityOrel++;
-
+                basketCoins.add(0);
+            } else {
+                quantityOrel++;
+                basketCoins.add(1);
+            }
         }
-       double probabilityOrel =(double) quantityOrel/quantityAttempt;
-        double probabilityReshka = (double) quantityReshka/quantityAttempt;
+        double probabilityOrel = (double) quantityOrel / quantityAttempt;
+        double probabilityReshka = (double) quantityReshka / quantityAttempt;
         String formattedDoubleOrel = String.format("%.2f", probabilityOrel);
         String formattedDoubleReshka = String.format("%.2f", probabilityReshka);
+        int count = 1;
+        int max = 1;
+        int maxCoin = 1;
+        for (int i = 0; i < basketCoins.size() - 1; i++) {
+            if (basketCoins.get(i).equals(basketCoins.get(i + 1))) {
+                count++;
+
+                if (max < count) {
+                    max = count;
+                    maxCoin = i;
+                }
+            } else
+                count = 1;
+        }
 
 
-        System.out.println((quantityOrel + quantityReshka)  + " - Количество бросков ");
-        System.out.println(quantityOrel +" - Количество Орлов. " +  "Фактическая вероятность " + formattedDoubleOrel);
-        System.out.println(quantityReshka +" - Количество Решек. "  +  "Фактическая вероятность " + formattedDoubleReshka);
+            System.out.println("Массив");
+            System.out.println(basketCoins);
+            System.out.println((quantityOrel + quantityReshka) + " - Количество бросков ");
+            System.out.println(quantityOrel + " - Количество Орлов. " + "Фактическая вероятность " + formattedDoubleOrel);
+            System.out.println(quantityReshka + " - Количество Решек. " + "Фактическая вероятность " + formattedDoubleReshka);
+            System.out.println(max +" - Самая длинная последовательность одинаковых результатов");
+
+            if(maxCoin == 0){
+                System.out.println("Самая длинная последовательность состоит из решек");
+            } else {
+                System.out.println("Самая длинная последовательность состоит из орлов");
+            }
+
+
+
+
+
+        }
+
+
     }
 
-
-}
